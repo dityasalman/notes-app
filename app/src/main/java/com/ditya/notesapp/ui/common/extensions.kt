bc02b97554d2886ebc3668c17898b6fae340b7eb
ditya <dityaasalman@gmail.com>
2025-02-25 17:16:34 +0700

    fix time stamp format

--- app/src/main/java/com/ditya/notesapp/ui/common/extensions.kt
+++ app/src/main/java/com/ditya/notesapp/ui/common/extensions.kt
@@ -1,6 +1,5 @@
 package com.ditya.notesapp.ui.common
 
-import android.util.Log
 import com.ditya.notesapp.db.entity.NotesEntity
 import com.ditya.notesapp.ui.detail_screen.DetailEvent
 import java.text.SimpleDateFormat
@@ -16,29 +15,28 @@ fun Long.formatTimestamp(): String {
 
     val calendarNow = Calendar.getInstance()
     val calendarTimestamp = Calendar.getInstance()
-    calendarTimestamp.timeInMillis = this // Ensure correct assignment
+    calendarTimestamp.timeInMillis = this
+
+    val dateFormatDayName = SimpleDateFormat("EEEE", Locale.getDefault()) // "Monday"
+    val dateFormatDayMonth = SimpleDateFormat("dd MMM", Locale.getDefault()) // "13 Feb"
+    val dateFormatMonthYear = SimpleDateFormat("MMM yyyy", Locale.getDefault()) // "Feb 2024"
 
     return when {
-        diff < TimeUnit.MINUTES.toMillis(1) -> "${diff / 1000} seconds ago" // Less than a minute
-        diff < TimeUnit.HOURS.toMillis(1) -> "${diff / TimeUnit.MINUTES.toMillis(1)} minutes ago" // Less than an hour
-        diff < TimeUnit.DAYS.toMillis(1) -> "${diff / TimeUnit.HOURS.toMillis(1)} hours ago" // Less than a day
-        calendarNow.get(Calendar.YEAR) == calendarTimestamp.get(Calendar.YEAR) -> {
-            when {
-                calendarNow.get(Calendar.DAY_OF_YEAR) - calendarTimestamp.get(Calendar.DAY_OF_YEAR) == 1 -> "Yesterday" // Yesterday
-                calendarNow.get(Calendar.WEEK_OF_YEAR) == calendarTimestamp.get(Calendar.WEEK_OF_YEAR) -> {
-                    SimpleDateFormat("EEEE", Locale.getDefault()).format(Date(this)) // Show day name (e.g., "Monday")
-                }
-                else -> SimpleDateFormat("dd MMM", Locale.getDefault()).format(Date(this)) // Show "13 Feb" for older dates in the same year
-            }
-        }
-        else -> SimpleDateFormat("MMM yyyy", Locale.getDefault()).format(Date(this)) // Show "Feb 2024" for different years
+        diff < TimeUnit.MINUTES.toMillis(1) -> "${diff / 1000} seconds ago"
+        diff < TimeUnit.HOURS.toMillis(1) -> "${diff / TimeUnit.MINUTES.toMillis(1)} minutes ago"
+        diff < TimeUnit.DAYS.toMillis(1) -> "${diff / TimeUnit.HOURS.toMillis(1)} hours ago"
+        calendarNow.get(Calendar.DAY_OF_YEAR) - calendarTimestamp.get(Calendar.DAY_OF_YEAR) == 1 &&
+                calendarNow.get(Calendar.YEAR) == calendarTimestamp.get(Calendar.YEAR) -> "Yesterday"
+        diff < TimeUnit.DAYS.toMillis(7) -> dateFormatDayName.format(Date(this)) // Show day name if within 7 days
+        calendarNow.get(Calendar.YEAR) == calendarTimestamp.get(Calendar.YEAR) -> dateFormatDayMonth.format(Date(this)) // Same year, show "13 Feb"
+        else -> dateFormatMonthYear.format(Date(this)) // Different year, show "Feb 2024"
     }
 }
 
 // Setting how the data will show up in the main screen
 fun previewFactory(note: NotesEntity): NotesEntity {
-    if (note.title.isNotBlank()) return note
-    else return note.copy(title = note.content.take(20), content = note.content.drop(20).take(100))
+    return if (note.title.isNotBlank()) note
+    else note.copy(title = note.content.take(20), content = note.content.drop(20).take(100))
 }
 
 // Setting if the data will be deleted or updated
